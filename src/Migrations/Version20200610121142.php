07b4046e916d177fa1a9d2ae81cf955ab3bef596
trinh-86068 <ttrinh@norsys.fr>
2020-06-10 14:44:42 +0200

    PARAM(BO): Migration - Ajout des 3 categories principales

---
+++ src/Migrations/Version20200610121142.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use App\Entity\Categorie;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200610121142 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return 'Ajout des 3 catégories principales';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $this->addSql("INSERT INTO categorie (titre) VALUES ('".Categorie::A_LA_UNE."')");
+        $this->addSql("INSERT INTO categorie (titre) VALUES ('".Categorie::VIE_DES_PROJETS."')");
+        $this->addSql("INSERT INTO categorie (titre) VALUES ('".Categorie::AGENDA."')");
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
